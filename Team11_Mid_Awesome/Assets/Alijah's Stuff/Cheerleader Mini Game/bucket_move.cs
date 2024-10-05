38279db4fa71741bc82206b37d9c103abb88d486
alijah1696 <alijah1696@gmail.com>
2024-10-04 22:11:22 -0400

    Cheerleader Game
    Added early prototype for game with blood spilling particles, bucket moving and cheerleader running around

--- Team11_Mid_Awesome/Assets/Alijah's Stuff/Cheerleader Mini Game/bucket_move.cs
+++ Team11_Mid_Awesome/Assets/Alijah's Stuff/Cheerleader Mini Game/bucket_move.cs
@@ -8,7 +8,7 @@ public class bucket_move : MonoBehaviour
 
 
     Rigidbody2D rb;
-    float moveSpeed = 5f;
+    public float moveSpeed = 10f;
     public Vector2 movement;
 
     // Start is called before the first frame update
@@ -19,6 +19,15 @@ public class bucket_move : MonoBehaviour
 
     // Update is called once per frame
     void FixedUpdate() {
+        
+        Vector2 screenPosition = Camera.main.WorldToViewportPoint(rb.position);
+        
+        float screenBound = 0.05f;
+        if(screenPosition.x < screenBound || screenPosition.x > (1f - screenBound)){
+            //bounds the bucket to the screen
+            rb.position = Camera.main.ViewportToWorldPoint(new Vector2(Mathf.Clamp(screenPosition.x, screenBound, (1f - screenBound)), screenPosition.y));
+        }
+
         movement.x = Input.GetAxisRaw("Horizontal");
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
--- Team11_Mid_Awesome/Assets/Alijah's Stuff/Cheerleader Mini Game/cheerleader_move.cs
+++ Team11_Mid_Awesome/Assets/Alijah's Stuff/Cheerleader Mini Game/cheerleader_move.cs
@@ -6,35 +6,50 @@ public class cheerleader_move : MonoBehaviour
 {
 
     Rigidbody2D rb;
-    float moveSpeed = 5f;
+    public float moveSpeed;
+    public float difficulty;
     float direction = 1f;
     
-
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        moveSpeed = 10f;
+        difficulty = 1f;
     }
 
     // Update is called once per frame
     void Update() {
         //changes direction of cheerleader
-        Vector2 screenPosition = Camera.main.WorldToViewportPoint(transform.position);
-    
+        Vector2 screenPosition = Camera.main.WorldToViewportPoint(rb.position);
+
         if(screenPosition.x < 0 || screenPosition.x > 1){
+            //bounds the chearleader to move left and right
+            rb.position = Camera.main.ViewportToWorldPoint(new Vector2(Mathf.Clamp(screenPosition.x, 0.0f, 1f), screenPosition.y));
             turnAround();
         }
-        transform.localRotation = Quaternion.Euler(0, (90f * direction - 90f), 0f);
+
+        float screenBound = 0.1f;
+        float screenRotLerp = 1f;
+        if(screenPosition.x < screenBound){
+            screenRotLerp = screenPosition.x/screenBound;
+        }else if(screenPosition.x > (1f - screenBound)){
+            screenRotLerp = (1f - screenPosition.x) / screenBound;
+        }
+        transform.localRotation = Quaternion.Euler(0, (90f * (direction * screenRotLerp) - 90f), 0f);
 
         //moves the position
         Vector2 movement = new Vector2(direction, 0f);
+
+        Debug.Log(rb.position + " : " + screenPosition + " -> " + Input.GetAxisRaw("Horizontal"));
+
         rb.MovePosition(rb.position + movement * moveSpeed * fixedTime());
+        
         
     }
 
     void turnAround(){
         direction = direction * -1;
-        //Debug.Log(screenPosition.x);
     }
     
 

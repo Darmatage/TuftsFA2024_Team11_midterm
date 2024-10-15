878728f095ff9b707bfb3c431ff4e358731a83ae
alijah1696 <alijah1696@gmail.com>
2024-10-14 22:28:38 -0400

    progress bar
    progress bar

--- Team11_Mid_Awesome/Assets/Scenes/Classroom/scripts/ProgressBarManager.cs
+++ Team11_Mid_Awesome/Assets/Scenes/Classroom/scripts/ProgressBarManager.cs
@@ -1,31 +1,37 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ProgressBarManager : MonoBehaviour
 {
     public static ProgressBarManager instance;
-    public Slider progressBar; // Or Image if you prefer using an Image UI
+    public float progressAmount;
+    public GameObject progressBar;
 
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject); // This ensures the object is not destroyed when changing scenes
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
-            Destroy(gameObject); // If another instance exists, destroy this one
+            Destroy(gameObject);
         }
     }
 
-    // Call this method to update the progress bar
+    public void Update(){
+        //progressbar.SetActive(SceneManager.GetActiveScene().name == "ClassroomBook");
+        progressBar.GetComponent<Renderer>().material.SetFloat("_progress", progressAmount);
+    }
+
     public void UpdateProgress(float progress)
     {
         if (progressBar != null)
         {
-            progressBar.value = progress; // Set the value between 0 and 1 for sliders
-            // For an Image: progressBar.fillAmount = progress;
+            progressAmount += progress;
         }
     }
 }
+
